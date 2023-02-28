684909cebbdcd2fbc816184ede5dfe849ced0b2b
AxelBaranowski3il <baranowa@3il.fr>
2023-02-28 14:06:21 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.mini_projet_java_fx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.mini_projet_java_fx to javafx.fxml;
+    exports com.example.mini_projet_java_fx;
+}
